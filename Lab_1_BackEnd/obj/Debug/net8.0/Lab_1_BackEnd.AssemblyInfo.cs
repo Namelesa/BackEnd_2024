0b3b3abcd22b59c22aad27543098b87a1ee8bb2b
maksimbilyk <pdo090318@gmail.com>
2024-09-17 14:56:36 +0300

    Add Docker Files

--- Lab_1_BackEnd/obj/Debug/net8.0/Lab_1_BackEnd.AssemblyInfo.cs
+++ Lab_1_BackEnd/obj/Debug/net8.0/Lab_1_BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1_BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e62e0df1ceeee599cf00d0a593d9a4f698d11b07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5738134d4a7e4eb0a307667a6e552efd0178aff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_1_BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_1_BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
